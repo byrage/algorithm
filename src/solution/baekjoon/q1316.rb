f570acf35bd066ee7efc1c5e175bb02afc8866df
Youngwoong Choi <byrage91@gmail.com>
2017-07-12 01:38:24 +0900

    Add baekjoon solutions

---
+++ src/solution/baekjoon/q1316.rb
@@ -0,0 +1,42 @@
+=begin
+문제
+그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다. 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만, aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
+
+단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 둘째줄부터 N개의 줄에 단어가 들어온다. 단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
+
+출력
+첫째 줄에 그룹 단어의 개수를 출력한다.
+
+예제 입력  복사
+3
+happy
+new
+year
+예제 출력  복사
+3
+=end
+class Q1316
+  questions = gets.chomp.to_i
+  cnt = 0
+  questions.times do
+    word = gets.chomp.to_s.chars
+    appear_alphas = Array.new
+    is_group_word = true
+    word.each_index do |i|
+      next if word[i] == word[i+1]
+
+      if appear_alphas.include?(word[i])
+        is_group_word = false
+        break
+      end
+
+      appear_alphas.push word[i]
+    end
+    cnt += 1 if is_group_word
+  end
+
+  puts cnt
+end
